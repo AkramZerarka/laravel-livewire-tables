e370b366ff8ebb9574393e7bb89fefbf188b6075
Zerarka Mohamed Ali Akram <akram.zerarka@gmail.com>
2023-07-24 10:52:01 +0200

    Fix id component

--- resources/views/components/table/empty.blade.php
+++ resources/views/components/table/empty.blade.php
@@ -1,7 +1,7 @@
 @aware(['component'])
 
 @php
-    $attributes = $attributes->merge(['wire:key' => 'empty-message-'.$component->id]);
+    $attributes = $attributes->merge(['wire:key' => 'empty-message-' . $component->__id]);
     $theme = $component->getTheme();
 @endphp
 
@@ -9,12 +9,13 @@
     <tr {{ $attributes }}>
         <td colspan="{{ $component->getColspanCount() }}">
             <div class="flex justify-center items-center space-x-2 dark:bg-gray-800">
-                <span class="font-medium py-8 text-gray-400 text-lg dark:text-white">{{ $component->getEmptyMessage() }}</span>
+                <span
+                    class="font-medium py-8 text-gray-400 text-lg dark:text-white">{{ $component->getEmptyMessage() }}</span>
             </div>
         </td>
     </tr>
 @elseif ($theme === 'bootstrap-4' || $theme === 'bootstrap-5')
-     <tr {{ $attributes }}>
+    <tr {{ $attributes }}>
         <td colspan="{{ $component->getColspanCount() }}">
             {{ $component->getEmptyMessage() }}
         </td>
